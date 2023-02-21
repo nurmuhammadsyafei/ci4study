867aaa78e2cef54ec8651a04829b7e694ed1d43f
syafei kantor <muhammad.syafei@komatsugroup.id>
2023-02-21 16:33:56 +0700

    16:34 21/2/2023

--- ci4app/app/Views/form.php
+++ ci4app/app/Views/form.php
@@ -1,5 +1,63 @@
 <!DOCTYPE html>
+<!doctype html>
 <html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <title>submit demo</title>
+    <style>
+        p {
+            margin: 0;
+            color: blue;
+        }
+
+        div,
+        p {
+            margin-left: 10px;
+        }
+
+        span {
+            color: red;
+        }
+    </style>
+    <script src="https://code.jquery.com/jquery-3.6.3.js"></script>
+</head>
+
+<body>
+
+    <p>Type 'correct' to validate.</p>
+    <form action="javascript:alert( 'success!' );" id="formtag">
+        <div>
+            <input type="text">
+            <input type="submit">
+        </div>
+    </form>
+    <span></span>
+
+    <script>
+        $("form").submit(function(event) {
+            if ($("input").first().val() === "correct") {
+                $("span").text("Validated...").show();
+                return;
+            }
+
+            $("span").text("Not valid!").show().fadeOut(1000);
+            event.preventDefault();
+        });
+    </script>
+
+</body>
+
+</html>
+
+
+
+
+
+
+
+
+<?php die(); ?><html lang="en">
 
 <head>
     <meta charset="UTF-8">
@@ -9,6 +67,20 @@
 </head>
 
 <body>
+
+
+
+    <form>
+        <input type="radio" name="foo" value="1" checked="checked" />
+        <input type="radio" name="foo" value="0" />
+        <input name="bar" value="xxx" />
+        <select name="this">
+            <option value="hi" selected="selected">Hi</option>
+            <option value="ho">Ho</option>
+    </form>
+
+
+
 
 </body>
 
